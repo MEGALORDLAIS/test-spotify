3c741b12c807c156922fd23b35fca6d0ea3fb820
Lais <lais060788@gmail.com>
2025-02-14 13:43:00 -0300

    header correction

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div>
+        <h2>Artistas populares</h2>
+        <a href="/">Mostrar Tudo</a>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
